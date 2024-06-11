5818490e4cda23b08de7f5ed09e0bb91c922fc37
Adam Nasrudin <adamnasrudin858@gmail.com>
2024-06-11 10:21:48 +0700

    unit test login

---
+++ app/modules/user/service/login_test.go
@@ -0,0 +1,72 @@
+package service
+
+import (
+	"errors"
+	"reflect"
+	"testing"
+
+	"github.com/adamnasrudin03/go-template/app/modules/user/payload"
+	"github.com/stretchr/testify/mock"
+)
+
+func (srv *UserServiceTestSuite) Test_userService_Login() {
+
+	reqEnv := map[string]string{
+		"USE_RABBIT": "false",
+	}
+	tests := []struct {
+		name     string
+		envVars  map[string]string
+		input    payload.LoginReq
+		mockFunc func(params payload.LoginReq)
+		wantRes  *payload.LoginRes
+		wantErr  bool
+	}{
+		{
+			name:    "failed get user",
+			envVars: reqEnv,
+			input: payload.LoginReq{
+				Username: "hello-world",
+				Password: "password123",
+			},
+			mockFunc: func(params payload.LoginReq) {
+				srv.repo.On("Login", mock.Anything, params).Return(nil, errors.New("failed get user")).Once()
+
+			},
+			wantRes: nil,
+			wantErr: true,
+		},
+		{
+			name:    "user not found",
+			envVars: reqEnv,
+			input: payload.LoginReq{
+				Username: "hello-world",
+				Password: "password123",
+			},
+			mockFunc: func(params payload.LoginReq) {
+				srv.repo.On("Login", mock.Anything, params).Return(nil, nil).Once()
+
+			},
+			wantRes: nil,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		srv.T().Run(tt.name, func(t *testing.T) {
+			srv.setupEnvTesting(tt.envVars)
+			defer srv.cleanupEnvTesting(t, tt.envVars)
+
+			if tt.mockFunc != nil {
+				tt.mockFunc(tt.input)
+			}
+			gotRes, err := srv.service.Login(srv.ctx, tt.input)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("userService.Login() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(gotRes, tt.wantRes) {
+				t.Errorf("userService.Login() = %v, want %v", gotRes, tt.wantRes)
+			}
+		})
+	}
+}
