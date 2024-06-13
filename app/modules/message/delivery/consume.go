2fa5e027286b45ab06929015856379ae8acc342d
Adam Nasrudin <adamnasrudin858@gmail.com>
2024-06-13 14:34:37 +0700

    handle if not use rabbit mq

--- app/modules/message/delivery/consume.go
+++ app/modules/message/delivery/consume.go
@@ -1,12 +1,18 @@
 package delivery
 
 import (
+	"net/http"
+
 	"github.com/adamnasrudin03/go-template/app/models"
+	"github.com/adamnasrudin03/go-template/pkg/helpers"
 	"github.com/gin-gonic/gin"
 )
 
 func (c *msgDelivery) Consume(ctx *gin.Context) {
-	for _, v := range models.QueueList {
-		go c.consumeRabbitMQ(v)
+	if c.Cfg.App.UseRabbitMQ {
+		for _, v := range models.QueueList {
+			go c.consumeRabbitMQ(v)
+		}
 	}
+	helpers.RenderJSON(ctx.Writer, http.StatusOK, "Request Success")
 }
