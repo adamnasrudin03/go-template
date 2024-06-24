66c9f7e33c631f9d17cdbcb2a1fd8dad6a478ecb
Adam Nasrudin <adamnasrudin858@gmail.com>
2024-06-24 10:50:39 +0700

    add comment

--- pkg/helpers/response_error.go
+++ pkg/helpers/response_error.go
@@ -46,38 +46,64 @@ func (e *ResponseError) Error() string {
 	return e.Err.Error()
 }
 
-// FormatValidationError func which holds errors during user input validation
+// FormatValidationError function is used to format validation errors that occur
+// during user input validation. It changes the default format of errors from
+// github.com/go-playground/validator/v10.
+//
+// This function iterates over the validation errors and constructs a formatted
+// error message. The error message is then translated into the Indonesian language
+// using the Translate function. The resulting error message is then returned as
+// a ResponseError struct.
+//
+// Parameters:
+// - err: The validation errors that occurred.
+//
+// Returns:
+// - error: The formatted validation error.
 func FormatValidationError(err error) error {
+	// Initialize variables to store the translated and untranslated error messages.
 	var (
 		msgIdn  string
 		msgEnUs string
 	)
 
+	// Iterate over the validation errors.
 	for _, e := range err.(validator.ValidationErrors) {
+		// Add a comma and space to the existing error message if it's not empty.
 		if msgEnUs != "" {
 			msgEnUs = fmt.Sprintf("%v, ", strings.TrimSpace(msgEnUs))
 		}
 
+		// Construct the error message based on the validation error.
 		if e.Tag() == "email" {
 			msgEnUs = msgEnUs + fmt.Sprintf("%v must be type %v", e.Field(), e.Tag())
 		} else {
 			msgEnUs = msgEnUs + fmt.Sprintf("%v is %v %v", e.Field(), e.Tag(), e.Param())
 		}
 
+		// If the parameter is not empty and the type is a string, add the word "character".
 		if e.Param() != "" && e.Type().Name() == "string" {
 			msgEnUs = msgEnUs + " character"
 		}
-
 	}
 
+	// Add a period to the end of the error message and remove any leading or trailing spaces.
 	msgEnUs = strings.TrimSpace(msgEnUs) + "."
+
+	// Set the target language to Indonesian.
 	langTo := language.Indonesian.String()
+
+	// Translate the error message to Indonesian using the Translate function.
 	msgIdn, errTranslate := Translate(msgEnUs, Auto, langTo)
+
+	// If there is an error during translation, set the translated message to the untranslated message
+	// and log the error.
 	if errTranslate != nil {
 		msgIdn = msgEnUs
 		log.Printf("Translate Text %v to %v error: %v \n", Auto, langTo, errTranslate)
 	}
 
+	// Return the formatted validation error as a ResponseError struct.
 	return NewError(ErrValidation, NewResponseMultiLang(MultiLanguages{
 		ID: msgIdn,
 		EN: msgEnUs,
